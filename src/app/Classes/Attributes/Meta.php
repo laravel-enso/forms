9959efb70667e85eb78157ba8c793afc9035a3f7
Adrian Ocneanu <aocneanu@gmail.com>
2018-08-16 06:12:07 +0000

    Apply fixes from StyleCI

--- src/app/Classes/Attributes/Meta.php
+++ src/app/Classes/Attributes/Meta.php
@@ -14,6 +14,6 @@ class Meta
     ];
 
     const Types = [
-        'input', 'select', 'datepicker', 'timepicker', 'textarea', 'password'
+        'input', 'select', 'datepicker', 'timepicker', 'textarea', 'password',
     ];
 }
