22a8c854f6250a8b376ae1e90a921e70a789d282
mishu <mishu@xtelecom.ro>
2018-10-20 13:33:50 +0300

    updates style.ci

--- src/app/Classes/Builder.php
+++ src/app/Classes/Builder.php
@@ -31,13 +31,13 @@ class Builder
 
     private function setValues()
     {
-        if (!$this->model) {
+        if (! $this->model) {
             return $this;
         }
 
         collect($this->template->sections)->each(function ($section) {
             collect($section->fields)->each(function ($field) {
-                if (!$this->dirty->contains($field->name)) {
+                if (! $this->dirty->contains($field->name)) {
                     $field->value = $this->value($field);
                 }
             });
@@ -99,11 +99,11 @@ class Builder
 
     private function appendConfigParams()
     {
-        if (!property_exists($this->template, 'authorize')) {
+        if (! property_exists($this->template, 'authorize')) {
             $this->template->authorize = config('enso.forms.authorize');
         }
 
-        if (!property_exists($this->template, 'dividerTitlePlacement')) {
+        if (! property_exists($this->template, 'dividerTitlePlacement')) {
             $this->template->dividerTitlePlacement = config('enso.forms.dividerTitlePlacement');
         }
 
@@ -112,7 +112,7 @@ class Builder
 
     private function dateFormat($field)
     {
-        if (!property_exists($field->meta, 'format')) {
+        if (! property_exists($field->meta, 'format')) {
             $field->meta->format = config('enso.forms.dateFormat');
         }
 
