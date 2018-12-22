7e14c7b943d44ea77aaca11367909f2629b38b2c
mishu <mishu@xtelecom.ro>
2018-12-22 19:42:07 +0200

    moves the fetch logic into VueForm; introduces EnsoForm

---
+++ src/resources/js/components/enso/vueforms/EnsoForm.vue
@@ -0,0 +1,89 @@
+<template>
+
+    <vue-form :i18n="__"
+        :locale="locale"
+        :params="params"
+        :path="path"
+        v-on="$listeners"
+        @loaded="ready = true; $emit('loaded')"
+        ref="form">
+        <template v-for="field in customFields"
+            :slot="field.name"
+            slot-scope="{ field, errors }">
+            <slot :name="field.name"
+                :field="field"
+                :i18n="__"
+                :locale="locale"
+                :errors="errors"/>
+        </template>
+        <slot name="actions" slot="actions"/>
+    </vue-form>
+
+</template>
+
+<script>
+
+import VueForm from '../../../components/enso/vueforms/VueForm.vue';
+
+export default {
+    name: 'EnsoForm',
+
+    components: { VueForm },
+
+    props: {
+        path: {
+            type: String,
+            default() {
+                return route(this.$route.name, this.$route.params, false);
+            },
+        },
+        params: {
+            type: Object,
+            default: null,
+        },
+        locale: {
+            type: String,
+            default() {
+                return this.$store.state.preferences.global.lang;
+            },
+        },
+    },
+
+    data: () => ({
+        ready: false,
+    }),
+
+    computed: {
+        data() {
+            return this.ready
+                ? this.$refs.form.data
+                : null;
+        },
+        customFields() {
+            return this.ready
+                ? this.$refs.form.customFields
+                : [];
+        },
+    },
+
+    methods: {
+        field(field) {
+            return this.ready
+                ? this.$refs.form.field(field)
+                : null;
+        },
+        param(param) {
+            return this.ready
+                ? this.$refs.form.param(param)
+                : null;
+        },
+        routeParam(param) {
+            return this.ready
+                ? this.$refs.form.routeParam(param)
+                : null;
+        },
+    },
+
+};
+
+</script>
