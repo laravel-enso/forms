8d393de8bc43e312f86eca6646c2b71859f0ed57
Adrian Ocneanu <aocneanu@gmail.com>
2019-08-25 11:23:27 +0000

    Apply fixes from StyleCI

--- src/app/Attributes/Meta.php
+++ src/app/Attributes/Meta.php
@@ -11,7 +11,7 @@ class Meta
         'disabled', 'readonly', 'hidden', 'source', 'format', 'time', 'rows',
         'placeholder', 'trackBy', 'label', 'tooltip', 'symbol', 'precision',
         'thousand', 'decimal', 'positive', 'negative', 'zero', 'resize',
-        'translated', 'time12hr', 'disable-clear', 'disable-filtering', 'params'
+        'translated', 'time12hr', 'disable-clear', 'disable-filtering', 'params',
     ];
 
     const Types = [
