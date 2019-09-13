30e1e6787a465e765ed2f6d88800e5ab64b009df
mishu <mishu@xtelecom.ro>
2019-09-13 12:46:47 +0300

    removes deprecated disable-filtering from meta

--- src/app/Attributes/Meta.php
+++ src/app/Attributes/Meta.php
@@ -7,11 +7,10 @@ class Meta
     const Mandatory = ['type'];
 
     const Optional = [
-        'options', 'multiple', 'custom', 'content', 'step', 'min', 'max',
-        'disabled', 'readonly', 'hidden', 'source', 'format', 'time', 'rows',
-        'placeholder', 'trackBy', 'label', 'tooltip', 'symbol', 'precision',
-        'thousand', 'decimal', 'positive', 'negative', 'zero', 'resize',
-        'translated', 'time12hr', 'disable-clear', 'disable-filtering', 'params',
+        'options', 'multiple', 'custom', 'content', 'step', 'min', 'max', 'disabled', 'readonly',
+        'hidden', 'source', 'format', 'time', 'rows', 'placeholder', 'trackBy', 'label', 'tooltip',
+        'symbol', 'precision', 'thousand', 'decimal', 'positive', 'negative', 'zero', 'resize',
+        'translated', 'time12hr', 'disable-clear', 'params',
     ];
 
     const Types = [
