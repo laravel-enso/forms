72905cb1f40870fa5ddd693ddfa0f7db66d68237
reza <raftx24@gmail.com>
2020-08-29 16:38:27 +0430

    adds encrypt type

--- src/Attributes/Fields.php
+++ src/Attributes/Fields.php
@@ -5,4 +5,6 @@ namespace LaravelEnso\Forms\Attributes;
 class Fields
 {
     public const List = ['label', 'name', 'value', 'meta'];
+
+    public const EncryptValue = '************************';
 }
